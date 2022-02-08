3595c42d7bdd59c3941e298b1f94df203affe9f3
ersenozturk <70ozturkersen@gmail.com>
2022-02-09 02:07:43 +0300

    add new item,delete an item and delete all item

---
+++ JS Dom-EventListener/script1.js
@@ -0,0 +1,69 @@
+// declaration of variables
+
+const form = document.getElementById('addTaskForm');
+const input = document.querySelector('#txtTaskName');
+const btnDeleteAll = document.getElementById('btnDeleteAll')
+const taskList = document.querySelector('#task-list');
+
+eventListener();
+
+function eventListener() {
+    
+    // addNewItem
+    form.addEventListener('submit',addNewItem);
+
+    // deleteItem
+    taskList.addEventListener('click',deleteItem)
+
+    //deleteAllItem
+    btnDeleteAll.addEventListener('click',deleteAllItem);
+}
+
+// ******************
+function addNewItem(e) {
+    
+    //prevent empty value
+    if (input.value === '') {
+        alert('You cannot empty value')
+    }
+
+    // create a
+    const newA = document.createElement('a')
+    newA.className = 'delete-item'
+    newA.setAttribute('href','#')
+    newA.innerHTML = '<i class="fas fa-times"></i>'
+    
+    // create li 
+    const newLi = document.createElement('li');
+    newLi.className = 'list-area-item';
+    newLi.appendChild(document.createTextNode(input.value));
+
+    // a append to li
+    newLi.appendChild(newA);
+
+    // li append to ul
+    taskList.appendChild(newLi);
+    
+    // after submit clear value
+    input.value = ''
+
+    // prevent reload by every submit
+    e.prevetDefault();
+}
+//***
+function deleteItem(e) {
+    if (e.target.className === 'fas fa-times') {
+        e.target.parentElement.parentElement.remove();
+    }
+    e.prevetDefault();
+}
+//***
+ function deleteAllItem(e) {
+    if (confirm('Are you really sure_?')) {
+        //  taskList.remove(); :)))))))))
+        // taskList.innerHTML = '' :))))))))
+        while (taskList.firstChild) {
+        taskList.firstChild.remove()
+        }
+    }
+}
