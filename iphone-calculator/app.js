2227c52db0183f49503fe459c2fc74e2cd815e26
ersenozturk <70ozturkersen@gmail.com>
2022-03-02 22:28:39 +0300

    app.js added for iphone-calculator

---
+++ iphone-calculator/app.js
@@ -0,0 +1,26 @@
+// Variables
+const previousElement = document.querySelector(".previous-display");
+const currentElement = document.querySelector(".current-display");
+
+const acButton = document.querySelector(".ac");
+const pmButton = document.querySelector(".pm");
+const percentButton = document.querySelector(".percent");
+
+const additionButton = document.querySelector(".addition");
+const subtractionButton = document.querySelector(".subtraction");
+const multiplicationButton = document.querySelector(".multiplication");
+const divisionButton = document.querySelector(".division");
+const equalsButton = document.querySelector(".equals");
+
+const decimalButton = document.querySelector(".decimal");
+const number0 = document.querySelector(".number-0");
+const number1 = document.querySelector(".number-1");
+const number2 = document.querySelector(".number-2");
+const number3 = document.querySelector(".number-3");
+const number4 = document.querySelector(".number-4");
+const number5 = document.querySelector(".number-5");
+const number6 = document.querySelector(".number-6");
+const number7 = document.querySelector(".number-7");
+const number8 = document.querySelector(".number-8");
+const number9 = document.querySelector(".number-9");
+
