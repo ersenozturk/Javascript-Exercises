a463614e4e886673d5817c35c338610ca19ac7e8
ersenozturk <70ozturkersen@gmail.com>
2022-03-09 18:52:13 +0300

    index.js added

---
+++ JSON-exercise1/index.js
@@ -0,0 +1,40 @@
+const showPersonnelBtn = document.querySelector('#show-personnel-btn')
+
+const personnelTable = document.querySelector('#personnel-table')
+
+showPersonnelBtn.addEventListener('click',loadPersonnel)
+
+function loadPersonnel() {
+
+    loading.style.display = 'block'
+
+    const req = new XMLHttpRequest();
+    req.open('GET','personels.json',true);
+
+    setTimeout(() => {
+
+        req.onload = function () {
+            loading.style.display = 'none'
+
+            if (this.status == 200) {
+                let personnels = JSON.parse(this.responseText);
+                let html = ''
+                personnels.forEach(personnel => {
+                    html += `
+                        <tr>
+                            <td>${personnel.firstName}</td>
+                            <td>${personnel.lastName}</td>
+                            <td>${personnel.age}</td>
+                            <td>${personnel.statu}</td>
+                            <td>${personnel.location}</td>
+                        </tr>
+                        ` 
+                });
+                personnelTable.innerHTML = html
+            } else {
+                alert(`Error ${req.status}`)
+            }
+        }
+        req.send()
+    }, 2000);
+}
