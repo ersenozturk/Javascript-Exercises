ff76d9980354fa070dc0ecc91e0d63eabd1710b7
ersen <70ozturkersen@gmail.com>
2022-06-26 12:37:15 +0300

    local storage added

--- todoAPP-localStorage/app.js
+++ todoAPP-localStorage/app.js
@@ -1,38 +1,99 @@
-const btn = document.getElementById("todo-button");
-const todoInput = document.getElementById("todo-input");
-const todoUl = document.getElementById("todo-ul");
+//* ======================================================
+//*                     TODO APP
+//* ======================================================
+//? Selectors
+const btn = document.getElementById('todo-button');
+const todoInput = document.getElementById('todo-input');
+const todoUl = document.getElementById('todo-ul');
 
-btn.addEventListener("click", (e) => {
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+renderSavedTodos();
+
+function renderSavedTodos() {
+  todos.forEach((todo) => {
+    createListElement(todo);
+  });
+}
+
+function createListElement(todo) {
+  //? her bir todo objesini destructure yaptık
+  const { id, content, isDone } = todo;
   todoUl.innerHTML += `
-    <li>
-        <i class="fa fa-check"></i>
-        <p>${todoInput.value}</p>
-        <i class="fa fa-trash"></i>
-    </li>
-    `;
-    todoInput.value = ''
+     <li id=${id} class=${isDone ? 'checked' : ''} >
+      <i class="fa fa-check"></i>
+      <p>${content}</p>
+      <i class="fa fa-trash"></i>
+    </li>`;
+}
 
+//? Baslangicta input aktif olsun
+window.onload = function () {
+  todoInput.focus();
+};
+
+//? Add Buton Event'inin tanimanmasi
+btn.addEventListener('click', () => {
+  if (!todoInput.value) {
+    alert('Please enter your todo');
+  } else {
+    const todoObject = {
+      id: new Date().getTime(),
+      isDone: false,
+      content: todoInput.value,
+    };
+
+    //?Yeni olsuturulan todo'yu diziye sakla
+    todos.push(todoObject);
+
+    //?todos dizisinin son halini localStorage'e sakla
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    createListElement(todoObject);
+    todoInput.value = '';
+  }
 });
 
+//? Klavyeden enter tusuna basilmasi ile add butonunun click fonksiyonunun cagrilmasi
+todoInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    btn.click();
+  }
+});
 
-todoInput.addEventListener('keydown', (e)=>{
-   if (e.code == 'Enter') {
-    btn.click()
-   }
-})
+//? delete ve ok butonları için event tanimlamasi
+todoUl.addEventListener('click', (e) => {
+  const id = e.target.parentElement.getAttribute('id');
+  //!Event, sil butonlarının birisinden geldi ise
+  if (e.target.classList.contains('fa-trash')) {
+    //? Dizinin ilgili elementini sildi
+    todos = todos.filter((todo) => todo.id != id);
 
+    //?todos dizisinin son halini localStorage'e sakla
+    localStorage.setItem('todos', JSON.stringify(todos));
 
-todoUl.addEventListener('click',(e)=>{
-    
-    if (e.target.classList.contains('fa-check')) {
-        if (e.target.parentElement.classList.contains('checked')) {
-            e.target.parentElement.classList.remove('checked')
-        } else {
-            e.target.parentElement.classList.add('checked')
-        }
-    } else if(e.target.classList.contains('fa-trash')) {
-        e.target.parentElement.remove()
+    //?DOM'daki ilgili li elementini sil
+    e.target.parentElement.remove();
+  }
+
+  //!Event, silme veya ok butonlarından geldi ise
+  if (e.target.classList.contains('fa-check')) {
+    // todos dizisindeki ilgili elementin isDone kismini güncelle
+    todos.map((todo, index) => {
+      if (todo.id == id) {
+        todos[index].isDone = !todos[index].isDone;
+      }
+    });
+
+    //?todos dizisinin son halini localStorage'e sakla
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    //? ilgili li elementinde checked adinda bir class varsa bunu sil (DOM)
+    if (e.target.parentElement.classList.contains('checked')) {
+      e.target.parentElement.classList.remove('checked');
+    } else {
+      //? ilgili li elementinde checked adinda bir class yoksa ekle
+      e.target.parentElement.classList.add('checked');
     }
-
-    
-})
+  }
+});
