be3852eb01a02876ccbb09f5560d429d0f65c9a3
ersen <70ozturkersen@gmail.com>
2022-06-26 12:20:25 +0300

    basic to do list created

---
+++ todoAPP-localStorage/app.js
@@ -0,0 +1,38 @@
+const btn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+
+btn.addEventListener("click", (e) => {
+  todoUl.innerHTML += `
+    <li>
+        <i class="fa fa-check"></i>
+        <p>${todoInput.value}</p>
+        <i class="fa fa-trash"></i>
+    </li>
+    `;
+    todoInput.value = ''
+
+});
+
+
+todoInput.addEventListener('keydown', (e)=>{
+   if (e.code == 'Enter') {
+    btn.click()
+   }
+})
+
+
+todoUl.addEventListener('click',(e)=>{
+    
+    if (e.target.classList.contains('fa-check')) {
+        if (e.target.parentElement.classList.contains('checked')) {
+            e.target.parentElement.classList.remove('checked')
+        } else {
+            e.target.parentElement.classList.add('checked')
+        }
+    } else if(e.target.classList.contains('fa-trash')) {
+        e.target.parentElement.remove()
+    }
+
+    
+})
